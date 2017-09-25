5dbac85e9b8c370ac4e0b6472ea9ddf5b2268397
ssy <siyuansyand@163.com>
2017-09-25 11:20:54 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/org/test/ssy/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/org/test/ssy/coolweather/ChooseAreaFragment.java
@@ -19,6 +19,7 @@ import org.litepal.crud.DataSupport;
 import org.test.ssy.coolweather.db.City;
 import org.test.ssy.coolweather.db.County;
 import org.test.ssy.coolweather.db.Province;
+import org.test.ssy.coolweather.gson.Weather;
 import org.test.ssy.coolweather.util.HttpUtil;
 import org.test.ssy.coolweather.util.Utility;
 
@@ -98,10 +99,18 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
